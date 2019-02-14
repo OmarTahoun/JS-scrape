4128414520f505db40ad43efe7f3b34f4350e6e9
OmarTahoun <omaraiz489@gmail.com>
2019-02-14 19:41:38 +0800

    scraping a new website(github)

--- script.js
+++ script.js
@@ -2,27 +2,29 @@
 const cheerio = require('cheerio')
 const jsonframe = require('jsonframe-cheerio')
 const got = require('got');
-
+const fs = require('fs');
 
 async function scrapsite() {
-    const url = 'https://stackoverflow.com/questions?pagesize=50&sort=newest/'
+    const url = 'https://github.com/trending'
     const html = await got(url) // Loading URL
     const $ = cheerio.load(html.body) //Getting html data
-
     jsonframe($) // initializing the plugin
 
-    let frame = {
-    	Questions: {
-    		_s: ".question-summary",  //selects the question
+    var frame = {
+    	Repositories: {
+    		_s: ".col-12.d-block.width-full.py-4.border-bottom",
     		_d: [{
-    			title: ".summary h3",  //get the question title
-    			description: ".summary .excerpt", // get question description
+    			name: "a",
+    			description: ".py-1 p",
+    			programming_Language: ".f6.text-gray.mt-2 .d-inline-block.mr-3 [itemprop=programmingLanguage]" ,
+          stars: ".f6.text-gray.mt-2 a"
     		}]
     	}
+
     };
 
 
-    console.log($('#mainbar #questions').scrape(frame, {
+    console.log($('.explore-content .repo-list').scrape(frame, {
         string: true
     }))
 }
